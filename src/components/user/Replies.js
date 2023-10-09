d30c1c1be22b7d0aae35038f2997954f5ac971b7
vikas gupta <vikasaggrawal700@gmail.com>
2023-10-09 13:53:42 +0530

    Fetch a single post

--- src/components/user/Replies.js
+++ src/components/user/Replies.js
@@ -2,37 +2,41 @@ import { Avatar, Divider, Flex, Text } from '@chakra-ui/react';
 import { BsThreeDots } from 'react-icons/bs';
 import Actions from './Actions';
 
-const Replies = () => {
+const Replies = ({ replies }) => {
   return (
     <>
-      <Flex gap={4} my="3" py="2" width="full">
-        <Avatar
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPfO37MK81JIyR1ptwqr_vYO3w4VR-iC2wqQ&usqp=CAU"
-          name={'name'}
-          size="sm"
-        />
-        <Flex flexDirection="column" gap="1" width="full">
-          <Flex justifyContent="space-between" alignItems="center" width="full">
-            <Text fontSize="sm" fontWeight="bold">
-              {'name'}
-            </Text>
-            <Flex alignItems="center" gap={4}>
-              <Text fontSize="sm" color="gray.light">
-                Time
+      {replies?.map((r, i) => (
+        <>
+          <Flex gap={4} my="3" py="2" width="full">
+            <Avatar src={r?.userId?.image} name={r?.userId?.name} size="sm" />
+            <Flex flexDirection="column" gap="1" width="full">
+              <Flex
+                justifyContent="space-between"
+                alignItems="center"
+                width="full"
+              >
+                <Text fontSize="sm" fontWeight="bold">
+                  {r?.userId?.name}
+                </Text>
+                <Flex alignItems="center" gap={4}>
+                  <Text fontSize="sm" color="gray.light">
+                    Time
+                  </Text>
+                  <BsThreeDots cursor="pointer" />
+                </Flex>
+              </Flex>
+              <Text textAlign="start" fontSize="md">
+                {r?.text}
               </Text>
-              <BsThreeDots cursor="pointer" />
+              <Actions />
+              <Text color="gray.light" fontSize="sm" textAlign="start">
+                Likes
+              </Text>
             </Flex>
           </Flex>
-          <Text textAlign="start" fontSize="md">
-            Comment
-          </Text>
-          <Actions />
-          <Text color="gray.light" fontSize="sm" textAlign="start">
-            Likes
-          </Text>
-        </Flex>
-      </Flex>
-      <Divider mb="3" />
+          <Divider mb="3" />
+        </>
+      ))}
     </>
   );
 };
--- src/pages/UserPost.js
+++ src/pages/UserPost.js
@@ -6,7 +6,6 @@ import {
   Flex,
   Image,
   Text,
-  useToast,
 } from '@chakra-ui/react';
 import NavBar from '../components/common/NavBar';
 
@@ -15,84 +14,121 @@ import { BsThreeDots } from 'react-icons/bs';
 import Actions from '../components/user/Actions';
 import { useState } from 'react';
 import Replies from '../components/user/Replies';
+import useToastBox from '../hooks/useToastBox';
+import useFetchApiCall from '../hooks/useFetchApiCall';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import Loader from '../components/common/Loader';
 
 const UserPost = () => {
-  const toast = useToast();
+  const postId = useParams().postId;
+  console.log(postId);
+  const { showToast } = useToastBox();
+  const { apiCall, loading } = useFetchApiCall();
+
   const [liked, setLiked] = useState(false);
+  const [post, setPost] = useState();
 
   const handleLike = () => {
     setLiked(!liked);
   };
+
   const copyURL = () => {
     const currentLocation = window.location.href;
     navigator.clipboard.writeText(currentLocation).then(() => {
-      toast({
-        title: 'URL is copyed',
-        description: currentLocation,
-        status: 'success',
-        duration: 3000,
-        isClosable: true,
-      });
+      showToast('URL is copyed', currentLocation);
     });
   };
+
+  const loadPost = async () => {
+    const response = await apiCall(`post/${postId}`);
+    if (response.success) {
+      setPost(response.post);
+    }
+  };
+
+  useEffect(() => {
+    loadPost();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [postId]);
+
   return (
     <>
       <NavBar />
-      <Flex>
-        <Flex alignItems="center" gap={2} width="full">
-          <Avatar
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPfO37MK81JIyR1ptwqr_vYO3w4VR-iC2wqQ&usqp=CAU"
-            name="name"
-            size="md"
-          />
-          <Flex alignItems="center">
-            <Text fontSize="sm" fontWeight="bold">
-              {'name'}
-            </Text>
-            <Image src={verified} w="4" h="4" ml="2" />
-          </Flex>
-        </Flex>
-        <Flex alignItems="center" gap={4}>
-          <Text fontSize="sm" color="gray.light">
-            Time
-          </Text>
-          <BsThreeDots cursor="pointer" onClick={copyURL} />
-        </Flex>
-      </Flex>
-      <Text my="3" textAlign="start">
-        About thread
-      </Text>
-      <Box
-        borderRadius={6}
-        overflow="hidden"
-        border="1px solid"
-        borderColor="gary.light"
-        position="relative"
-      >
-        <Image src={`https://bit.ly/dan-abramov`} name={`name`} w="full" />
-      </Box>
-      <Flex gap={3} my={1} cursor="pointer">
-        <Actions liked={liked} handleLikeAndUnlike={handleLike} />
-      </Flex>
-      <Flex alignItems="center" gap="2">
-        <Text color="gray.light" fontSize="sm">
-          125 replies
-        </Text>
-        <Box width={0.5} height={0.5} bg={'gray.light'} borderRadius={'full'} />
-        <Text color="gray.light" fontSize="sm">
-          125 likes
-        </Text>
-      </Flex>
-      <Divider my="4" />
-      <Flex justifyContent="space-between">
-        <Flex gap={2}>
-          <Text fontSize={'2xl'}>👏</Text>
-          <Text color="gray.light">Get the app to like, reply and post.</Text>
-        </Flex>
-        <Button>Get</Button>
-      </Flex>
-      <Divider my="4" />
-      <Replies />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {post && (
+            <>
+              <Flex>
+                <Flex alignItems="center" gap={2} width="full">
+                  <Avatar
+                    src={post?.postedBy?.image}
+                    name={post?.postedBy?.name}
+                    size="md"
+                  />
+                  <Flex alignItems="center">
+                    <Text fontSize="sm" fontWeight="bold">
+                      {post?.postedBy?.name}
+                    </Text>
+                    <Image src={verified} w="4" h="4" ml="2" />
+                  </Flex>
+                </Flex>
+                <Flex alignItems="center" gap={4}>
+                  <Text fontSize="sm" color="gray.light">
+                    Time
+                  </Text>
+                  <BsThreeDots cursor="pointer" onClick={copyURL} />
+                </Flex>
+              </Flex>
+              <Text my="3" textAlign="start">
+                {post?.text}
+              </Text>
+              <Box
+                borderRadius={6}
+                overflow="hidden"
+                border="1px solid"
+                borderColor="gary.light"
+                position="relative"
+              >
+                {post?.img && (
+                  <Image src={post?.img} name={post?.postedBy?.name} w="full" />
+                )}
+              </Box>
+              <Flex gap={3} my={1} cursor="pointer">
+                <Actions liked={liked} handleLikeAndUnlike={handleLike} />
+              </Flex>
+              <Flex alignItems="center" gap="2">
+                <Text color="gray.light" fontSize="sm">
+                  {post?.replies?.length} replies
+                </Text>
+                <Box
+                  width={0.5}
+                  height={0.5}
+                  bg={'gray.light'}
+                  borderRadius={'full'}
+                />
+                <Text color="gray.light" fontSize="sm">
+                  {post?.likes?.length} likes
+                </Text>
+              </Flex>
+              <Divider my="4" />
+              <Flex justifyContent="space-between">
+                <Flex gap={2}>
+                  <Text fontSize={'2xl'}>👏</Text>
+                  <Text color="gray.light">
+                    Get the app to like, reply and post.
+                  </Text>
+                </Flex>
+                <Button>Get</Button>
+              </Flex>
+              <Divider my="4" />
+              <Replies replies={post?.replies} />
+            </>
+          )}
+        </>
+      )}
     </>
   );
 };
